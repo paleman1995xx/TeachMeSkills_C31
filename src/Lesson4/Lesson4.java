7ea0e2a0d081bbd4f0bfd3cda9f3ebc267d6910c
paleman1995xx <idSoll13@gmail.com>
2024-08-15 15:22:33 +0300

    full tasks lesson4

--- src/Lesson4/Lesson4.java
+++ src/Lesson4/Lesson4.java
@@ -1,5 +1,7 @@
 package Lesson4;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Lesson4 {
@@ -8,7 +10,11 @@ public class Lesson4 {
         System.out.print("Введите размер массива: ");
         int size = sc.nextInt();
         int[] array = new int[size];
-        array = new int[]{0, 1, 4, 3, 4, 5, 6, 7, 8, 9};
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(20);
+        }
+        System.out.println(Arrays.toString(array));
 
         System.out.print("Выберите задачу - ");
 
@@ -33,7 +39,9 @@ public class Lesson4 {
                 progression(array);
                 break;
             case 7:
-
+                difficultTask(new int[]{1, 2});
+                difficultTask(new int[]{1, 9});
+                difficultTask(new int[]{9, 9});
                 break;
             default:
                 System.out.print("Задача не найдена");
@@ -48,7 +56,6 @@ public class Lesson4 {
         for (int i = array.length - 1; i >= 0; i--) {
             System.out.print(array[i] + " ");
         }
-
     }
 
     private static void maxMin(int[] array) {
@@ -80,7 +87,6 @@ public class Lesson4 {
                 max = array[i];
                 indexMax = i;
             }
-
         }
         System.out.println("Минимальное значение: " + indexMin);
         System.out.println("Максимальное значение: " + indexMax);
@@ -89,7 +95,9 @@ public class Lesson4 {
     private static void matching(int[] array) {
         int quantity = 0;
         for (int number : array) {
-            if (number == 0) quantity++;
+            if (number == 0) {
+                quantity++;
+            }
         }
         System.out.print(quantity == 0 ? "Нулевых элементов нет." : "Количество нулевых элементов: " + quantity);
     }
@@ -108,54 +116,28 @@ public class Lesson4 {
     private static void progression(int[] array) {
         boolean isProgression = true;
         for (int i = 0; i < array.length - 1; i++) {
-            if (array[i] > array[i + 1])  {
+            if (array[i] > array[i + 1]) {
                 isProgression = false;
                 break;
-            };
+            }
         }
-        if (isProgression) {
-            System.out.print("Прогрессия возрастающая");
+        System.out.print(isProgression ? "Прогрессия возрастающая" : "Прогрессия не возрастающая");
+    }
+
+    private static void difficultTask(int[] array) {
+        StringBuilder line = new StringBuilder();
+        for (int j : array) {
+            line.append(j);
         }
-        else {
-            System.out.print("Прогрессия не возрастающая");
+
+        int number = Integer.parseInt(line.toString());
+        number++;
+        line = new StringBuilder(String.valueOf(number));
+
+        array = new int[line.length()];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Character.getNumericValue(line.charAt(i));
         }
+        System.out.println(Arrays.toString(array));
     }
 }
-
-
-
-//public static int(int min, int max) {
-// (int) (Math.random() * (max - min)) + min;
-
-/*
-* Для всех задач исходные условия следующие: пользователь с клавиатуры вводит размер
-массива (просто целое число). После того, как размер массива задан, заполнить его
-одним из двух способов: используя Math.random(), или каждый элемент массива вводится
-пользователем вручную. Попробовать оба варианта. После заполнения массива
-данными, решить для него следующие задачи:
-Задача 1:
-Пройти по массиву, вывести все элементы в прямом и в обратном порядке.
-Задача 2:
-Найти минимальный-максимальный элементы и вывести в консоль.
-Задача 3:
-Найти индексы минимального и максимального элементов и вывести в консоль.
-Задача 4:
-Найти и вывести количество нулевых элементов. Если нулевых элементов нет - вывести
-сообщение, что их нет.
-Задача 5:
-Пройти по массиву и поменять местами элементы первый и последний, второй и
-предпоследний и т.д.
-Задача 6:
-Проверить, является ли массив возрастающей последовательностью (каждое следующее
-число больше предыдущего).
-Задача *:
-Имеется массив из неотрицательных чисел(любой). Представьте что массив
-представляет целое число (Например массив {1,2,3} -> 123, {9,9,9} -> 999). Задача
-добавить единицу к этому “числу” и на выходе получить исправленный массив. Массив не
-содержит нуля в начале, кроме самого числа 0.
-Пример:
-Input: [1,4,0,5,6,3]
-Output: [1,4,0,5,6,4]
-Input: [9,9,9]
-Output: [1,0,0,0]*/
-
